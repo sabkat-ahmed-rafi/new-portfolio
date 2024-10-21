d0120fb8f7e322087c76b364565989daa59af6e7
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-10-22 02:57:21 +0600

    UI: border removed from cursor checker

--- app/components/others/MouseChecker.tsx
+++ app/components/others/MouseChecker.tsx
@@ -2,7 +2,7 @@ import Spline from '@splinetool/react-spline/next';
 
 export default function MouseChecker() {
   return (
-    <main  className='border absolute top-0 right-0 left-0 z-0'>
+    <main  className='absolute top-0 right-0 left-0 z-0 hidden lg:flex'>
       <Spline
         scene="https://prod.spline.design/uxNmdU3c2oelNzBV/scene.splinecode" 
       />
