ed3f3ded207cd59bf62bf1303765ec8d9afb8ec6
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-10-26 00:05:03 +0600

    A simple joke added

--- app/components/others/Skill.tsx
+++ app/components/others/Skill.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Skill = () => {
   return (
-    <section className='md:mx-[85px] mx-[5px] lg:mt-[200px] mt-10'>
+    <section className='md:mx-[85px] mx-[5px] lg:mt-[150px] mt-10'>
       <h1 className='md:text-3xl text-xl font-extrabold text-slate-700'>Skills</h1>
     </section>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import { VelocityScroll } from "./components/magicui/scroll-based-velocity";
 import Navbar from "./components/NavBar/NavBar";
 import Intro from "./components/others/Intro";
 import MouseChecker from "./components/others/MouseChecker";
-import Skill from "./components/others/skill";
+import Skill from "./components/others/Skill";
 
 export default function Home() {
   return (
@@ -9,6 +10,13 @@ export default function Home() {
       <Navbar />
       <MouseChecker />
       <Intro />
+      <div className="lg:mt-60 md:mt-0 mt-5">
+      <VelocityScroll
+      text="I used to have a life… then I started coding."
+      default_velocity={5}
+      className="font-display text-center text-4xl font-bold tracking-[-0.02em] text-blace drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
+    />
+      </div>
       <Skill />
    </section>
   );
