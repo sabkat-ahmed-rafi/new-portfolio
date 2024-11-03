c3d6ad845a299bc255f418345a685816e83dab2f
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-11-03 21:51:48 +0600

    Update: used the nextjs Image tag

--- app/components/Education/Education.tsx
+++ app/components/Education/Education.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Education = () => {
@@ -9,7 +10,7 @@ const Education = () => {
             <h1 className='md:text-3xl text-xl font-extrabold  text-slate-700'>Education</h1>
             </div>
             <div className='flex items-center justify-normal lg:justify-center space-x-4'>
-                <img className='w-[100px]' src="/images/collegeLogo.png" alt="College Logo" />
+                <Image className='w-[100px]' src="/images/collegeLogo.png" alt="College Logo" />
                 <div>
                 <p className='text-2xl font-semibold'>Government Bangla College</p>
                 <h1 className='text-slate-700'>Bachelor of Business Administration (2023-24)</h1>
