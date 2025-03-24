4261fe64dcf645ab13dd07fa4b31bf3b2fbcde64
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-03-24 19:17:45 +0600

    Add: animate the Intro

--- app/components/others/Intro.tsx
+++ app/components/others/Intro.tsx
@@ -1,15 +1,65 @@
+"use client";
+
 import React from 'react'
 import {WordRotate} from "@/components/magicui/word-rotate";
 import ShimmerButton from "@/components/magicui/shimmer-button";
 import ShimmerButton2 from "@/components/magicui/shimmer-button2";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
-
+gsap.registerPlugin(useGSAP);
 
 const Intro = () => {
+
+  useGSAP(() => {
+      const tl = gsap.timeline();
+
+      tl
+      .fromTo(
+        ".I", 
+        { y: -400, x: 150 }, 
+        { 
+          y: 0, 
+          x:0,
+          duration: 2, 
+          ease: "elastic.inOut"
+        }, 0)
+
+      .fromTo(
+        [".left-element", ".right-element"],
+        { x: 0 }, 
+        {
+          x: (i) => (i === 0 ? "-=50" : "+=50"), // Left goes left, right goes right
+          duration: 0.7,
+          ease: "elastic.inOut"
+        }, 1.5)
+
+      .to(".M", {
+          rotation: 360,
+          duration: 2,
+          ease: "elastic.inOut"
+        }, 2)
+
+      .to(
+        [".left-element", ".right-element"], 
+        { 
+          x: 0,
+          duration: 1,
+          ease: "elastic.inOut"
+        }, 3);
+
+  }), [];
+
   return (
     <>
         <section className='flex flex-col justify-center items-center  space-y-2 text-center md:mx-auto lg:mt-10'>
-            <h1 className='font-lilita font-extrabold  text-[100px] dark:text-white'>Hi, I&apos;m Rafi</h1>
+            <h1 className='font-lilita font-extrabold  text-[100px] dark:text-white'>
+              <span className='inline-block left-element'>Hi, I&apos;</span> 
+              <span className='inline-block M'>m</span>
+              <span className='inline-block right-element sm:pl-5'>Raf
+                <span className='inline-block I'>i</span>
+              </span>
+            </h1>
             <h3 className='text-4xl  font-extralight font-serif text-slate-800'>
             <WordRotate
                 className="text-4xl  font-extralight font-serif text-slate-800 dark:text-white"
