7b479fdb6e4e8047c09936af4b929b25a13e0bfe
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-05-24 23:21:10 +0600

    UI: add particles to the UI

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import MouseRing from "./components/MouseRing/MouseRing";
 import Navbar from "./components/NavBar/NavBar";
 import Intro from "./components/others/Intro";
 import Skill from "./components/others/Skill";
+import Particle from "./components/Particle/Particle";
 import Projects from "./components/project/Projects";
 
 export default function Home() {
@@ -26,6 +27,7 @@ export default function Home() {
       <EmailBox />
       <Footer />
       <MouseRing />
+      <Particle />
    </section>
   );
 }
