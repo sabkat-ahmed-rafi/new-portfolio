6b23a8f3db089a1f42bb2012b1fd32815539b398
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-08-24 01:19:07 +0600

    hide the splash cursor for mobile devices

--- app/components/SplashCursor/SplashCursor.tsx
+++ app/components/SplashCursor/SplashCursor.tsx
@@ -1518,7 +1518,7 @@ export default function SplashCursor({
   ]);
 
   return (
-    <div className="fixed top-0 left-0 z-50 pointer-events-none w-full h-full">
+    <div className="fixed top-0 left-0 z-50 pointer-events-none w-full h-full hidden md:flex">
       <canvas ref={canvasRef} id="fluid" className="w-screen h-screen block"></canvas>
     </div>
   );
