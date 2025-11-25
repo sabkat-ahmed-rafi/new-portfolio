1d2829793c672f70bd89eea423b8d0474312658a
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-11-25 12:36:10 +0600

    Update the education section

--- app/components/Education/Education.tsx
+++ app/components/Education/Education.tsx
@@ -12,7 +12,7 @@ const Education = () => {
                 <img className='w-[180px] md:w-[250px]' src="/images/collegeLogo.png" alt="College Logo" />
                 <div className='text-center'>
                 <p className='text-2xl font-semibold'>Government Bangla College</p>
-                <h1 className='text-slate-700'>Bachelor of Business Administration (2023-24)</h1>
+                <h1 className='text-slate-700'>Bachelor of Business Administration</h1>
                 <p className='text-slate-700 font-semibold'>Marketing</p>
                 </div>
             </div>
